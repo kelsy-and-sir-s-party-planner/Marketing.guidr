a8816abbf12a9c23cf5f2aa9adc24b3528ec35e0
Sir-Williams <sirjohnwilliams@live.com>
2019-05-19 14:09:19 -0700

    new party btn now toggles whole new party section; reactive for tablet

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-class NewParty{
-    constructor(newElement){
-        this.newElement = newElement;
-
-
-    }
-}
-
 
 class ShopDropDown{
     constructor(element){
@@ -28,37 +20,47 @@ class ShopDropDown{
 
 }
       
-let dropdowns = document.querySelectorAll('#shop-nav').forEach( dropdown => new ShopDropDown(dropdown));
+let shopdropdowns = document.querySelectorAll('#shop-nav').forEach( shopdropdown => new ShopDropDown(shopdropdown));
+
+class NewParty {
+    constructor(element) {
+      
+      this.element = element;
+      
+      this.button = document.querySelector('.new-party-btn');
+      
+      this.content = document.querySelector('.party-container');
+      
+      this.button.addEventListener('click', () => {
+        this.toggleContent();
+      })
+    }
+  
+    toggleContent() {
+    
+        this.content.classList.toggle('#new-party');
+        this.content.classList.toggle('party-hidden');
+    }
+  }
+  
+  let newpartys = document.querySelectorAll('#new-party').forEach( newparty => new NewParty(newparty));
 
 class TabLink {
     constructor(tabElement){
-      // assign this.tabElement to the tabElement DOM reference
-      // this.tabElement;
-      this.tabElement = tabElement;
       
-      // Get the `data-tab` value from this.tabElement and store it here
-      // this.tabData = ; 
+      this.tabElement = tabElement;
+       
       this.tabData = this.tabElement.dataset.tab;
       
-      // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-      
-      // Check to see if this.tabData is equal to 'all'
       if(this.tabData === 'all'){
-        // If `all` is true, select all cards regardless of their data attribute values
-        // this.cards = ;
+        
         this.cards = document.querySelectorAll('.selection-item');
       } else {
-        // else if `all` is false, only select the cards with matching this.tabData values
-        // this.cards = ;
+       
         this.cards = document.querySelectorAll(`.selection-item[data-tab='${this.tabData}']`);
       }
       
-       // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-      // this.cards = Array.from(this.cards).map();
       this.cards = Array.from(this.cards).map(card => new TabCard(card));
-  
-      // Add a click event that invokes this.selectTab
-      // this.tabElement.addEventListener();
   
       this.tabElement.addEventListener('click', () => {
         this.selectTab();
@@ -69,31 +71,20 @@ class TabLink {
   
     selectTab(){
   
-      // Select all elements with the .tab class on them
-      // const tabs = document.querySelectorAll();
       const tabs = document.querySelectorAll('.tab')
-      
-      // Iterate through the NodeList removing the .active-tab class from each element
-      // tabs.forEach()
+
       tabs.forEach((tab) => {
         tab.classList.remove('active-tab');
         console.log('class removed')
       })
-  
-      // Select all of the elements with the .card class on them
-      // const cards = ;
+ 
       const cards = document.querySelectorAll('.selection-item');
-  
-      // Iterate through the NodeList setting the display style each one to 'none'
-      // cards.forEach()
   
       cards.forEach(card => {
         card.style.display = 'none';
         console.log('changed to no display');
       })
       
-      // Add a class of ".active-tab" to this.tabElement
-      // this.tabElement;
       this.tabElement.classList.add('active-tab');
     
       this.cards.forEach(card => card.selectCard());
